0cfe7d2540018380700917e5a8ca59d16458f049
Cimer17 <ivanklopov2000@yandex.ru>
2023-02-17 18:28:09 +0300

    apple

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from website import app
 import threading
 
 
+
 if __name__ == '__main__':
     thread_syte = threading.Thread(target=bot.bot.polling, args=('127.0.0.1', 8000, True))
     thread_bot = threading.Thread(target=app.run)
