b7cb99d1c3ce6fa091a435666cf857c30f3162ac
Cimer17 <ivanklopov2000@yandex.ru>
2023-02-17 18:38:16 +0300

    gogo

--- website.py
+++ website.py
@@ -1,3 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def application():
+    return render_template('application.html')
+
+@app.route("/hr")
+def hr():
+    return render_template('hr.html')
