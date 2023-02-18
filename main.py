d726397ba8f21d2f9462da3a03618ce80fb712f4
Cimer17 <ivanklopov2000@yandex.ru>
2023-02-18 11:14:40 +0300

    штш

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-import bot
+import bot, time, os
 from website import app
 import threading
 
 
 if __name__ == '__main__':
-    thread_syte = threading.Thread(target=app.run)
-    thread_syte.start()
-    bot.bot.polling()
+    try:
+        os.system("pip install -r requirements.txt")
+        thread_syte = threading.Thread(target=app.run)
+        thread_syte.start()
+        bot.bot.polling()
+    except Exception as ex:
+        input(ex)
+        time.sleep(3)
